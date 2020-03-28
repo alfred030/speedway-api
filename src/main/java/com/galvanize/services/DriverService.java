9ae58375ae7712b57c9ba31c28fcaeeacb94dae3
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-27 23:35:35 -0700

    driver service complete
    all tests pass

--- src/main/java/com/galvanize/services/DriverService.java
+++ src/main/java/com/galvanize/services/DriverService.java
@@ -1,9 +1,14 @@
 package com.galvanize.services;
 
+import com.galvanize.entites.Car;
 import com.galvanize.entites.Driver;
 import com.galvanize.repository.DriverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DriverService {
@@ -13,11 +18,53 @@ public class DriverService {
     DriverRepository driverRepository;
 
 
+    //CREATE
+
+
     public Driver save(Driver driver) {
         if(driver != null){
             return driverRepository.save(driver);
         }else{
             throw new RuntimeException("driver should not be null");
         }
+    }
+
+
+    //READ
+
+
+    public List<Driver> findAllDrivers() {
+        return driverRepository.findAll();
+    }
+
+    public Driver findDriverById(Long id) {
+        if(driverRepository.findById(id).isPresent()){
+            return driverRepository.findById(id).get();
+        }else{
+            throw new RuntimeException("Driver id not found");
+        }
+    }
+
+
+    //UPDATE
+
+
+    public Driver updateDriverById(Long id, Driver driver) {
+        if(driverRepository.findById(id).isPresent()){
+            driverRepository.save(driver);
+            return driverRepository.findById(id).get();
+        }else{
+            throw new RuntimeException("Driver id not found");
+        }
+    }
+
+
+    public void deleteDriverById(Long id) {
+        if(driverRepository.findById(id).isPresent()){
+            driverRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("Driver id not found");
+        }
+
     }
 }
--- src/test/java/com/galvanize/services/DriverServiceTest.java
+++ src/test/java/com/galvanize/services/DriverServiceTest.java
@@ -1,6 +1,9 @@
 package com.galvanize.services;
 
+import com.galvanize.entites.Car;
 import com.galvanize.entites.Driver;
+import com.galvanize.entites.Model;
+import com.galvanize.entites.Status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
 public class DriverServiceTest {
+
+
     @Autowired
     DriverService driverService;
 
+
     private Driver driver;
+    private Car car;
 
     @BeforeEach
     public void setup() {
@@ -29,7 +37,13 @@ public class DriverServiceTest {
         String lastName = "name2";
         String nickName = "nickName";
         driver = new Driver(firstName, lastName, Date.valueOf(LocalDate.of(1990,11,16)), nickName);
+        driverService.save(driver);
+        car = new Car("Green Hornet", Model.Alpine, "1960", Status.AVAILABLE, 198L);
     }
+
+
+    //CREATE
+
 
     @Test
     public void saveDriverTest() {
@@ -38,6 +52,68 @@ public class DriverServiceTest {
         //Assert
         assertEquals(driver.getAge(), actual.getAge());
         assertEquals(driver.getNickName(), actual.getNickName());
+        //Teardown
+    }
+
+
+    //READ
+
+
+    @Test
+    public void getAllDriversTest() {
+        //Setup
+        int notExpected = 0;
+        List<Driver> drivers = driverService.findAllDrivers();
+        int actual = drivers.size();
+        //Exercise
+        //Assert
+        assertNotEquals(notExpected, actual);
+        //Teardown
+    }
+
+    @Test
+    public void getDriverByIdTest() {
+        //Setup
+        Driver expected = driver;
+        //Exercise
+        Driver actual = driverService.findDriverById(driver.getId());
+        //Assert
+        assertEquals(expected.hashCode(), actual.hashCode());
+        //Teardown
+    }
+
+
+    //UPDATE
+
+    @Test
+    public void updateDriverTest() {
+        //Setup
+        driver.setBirthDate(Date.valueOf(LocalDate.of(1991, 11, 16)));
+        driver.setCar(car);
+        driver.setFirstName("Batman");
+        driver.setLastName("Spiderman");
+        driver.setNickName("MickyMouse");
+        //Exercise
+        Driver driver2 = driverService.updateDriverById(driver.getId(), driver);
+        Car expected = car;
+        Car actual = driver2.getCar();
+        //Assert
+        assertEquals(expected.hashCode(), actual.hashCode());
+        //Teardown
+    }
+
+
+    // DELETE
+
+
+    @Test
+    public void deleteDriverTest() {
+        //Setup
+        Long id = driver.getId();
+        //Exercise
+        driverService.deleteDriverById(id);
+        //Assert
+        assertThrows(RuntimeException.class, () -> driverService.findDriverById(id));
         //Teardown
     }
 
